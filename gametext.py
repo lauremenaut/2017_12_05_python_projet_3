8c8ea127b70c694babcda0d3444b3396a99f3ff6
Laure <lauremenaut@yahoo.fr>
2017-12-28 00:09:41 -0500

    continuer = False at the end of the game

--- gametext.py
+++ gametext.py
@@ -6,16 +6,14 @@
 This class manages the "textual version" of the game "Help MacGyver to
 escape from the labyrinth !"
 McGyver needs to find and pick up 3 items (a needle, a small plastic
-tube and ether) in the labyrinth before neutralizing the guard and being
-able to escape.
+tube and ether) in the labyrinth to make a syringe. Then, he will be
+able to neutralize the guard and escape.
 
 """
 
-from labyrinth import *
-from macgyver import *
-from guard import *
-from syringe import *
-from position import *
+from labyrinth import Labyrinth
+from macgyver import MacGyver
+from syringe import Syringe
 
 
 class GameText:
@@ -23,24 +21,25 @@ class GameText:
         self.labyrinth = Labyrinth()
         self.macgyver = MacGyver(self.labyrinth)
         self.syringe = Syringe(self.labyrinth)
-        # self.guard = Guard() # à supprimer ?
 
     def start(self):
         continuer = True
-        print("Help MacGyver to escape from the labyrinth : he needs to pick \
+        print("\nHelp MacGyver to escape from the labyrinth : he needs to pick \
 up a needle, a tube and ether before fighting the guard !")
-        while continuer:
+        while continuer is not False:
             self.labyrinth.display()
             user_answer = input("In which direction do you want MacGyver to \
 move ? Please enter 'u' for up, 'd' for down, 'l' for left and 'r' for \
 right, or 'q' to quit : ")
             if user_answer not in ["q", "u", "d", "l", "r"]:
-                print("Invalid choice !")
+                print("\nInvalid choice !")
                 continue
             elif user_answer == "q":
                 continuer = False
             else:
-                self.macgyver.move(user_answer)
+                continuer = self.macgyver.move(user_answer)
+                if not continuer:
+                    self.labyrinth.display()
 
 
 def main():
