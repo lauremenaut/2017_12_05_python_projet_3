8278b35a1ca05a6677fa145c964cd542a7671000
Laure <lauremenaut@yahoo.fr>
2018-01-06 00:03:28 -0500

    Added icon and caption - Changed background image - x/0 and y/1 are now set correctly to refer positions - Added KEYDOWN events - Managed disappearance of collected items - Added success and failure images at the end of the game, and time.wait()

--- gamegui.py
+++ gamegui.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-""" Sets GameGUI class
+""" Sets GameGUI class.
 
-This class manages the "graphical version" of the game "Help MacGyver to escape
-from the labyrinth !"
+This class manages the "graphical version" of the game "Help MacGyver
+to escape from the labyrinth !"
 McGyver needs to find and pick up 3 items (a needle, a small plastic
 tube and ether) in the labyrinth to make a syringe. Then, he will be
 able to neutralize the guard and escape.
@@ -14,9 +14,9 @@ able to neutralize the guard and escape.
 import pygame
 from pygame.locals import *
 
-from labyrinth_GUI import Labyrinth
-from macgyver_GUI import MacGyver
-from syringe_GUI import Syringe
+from labyrinth import Labyrinth
+from macgyver import MacGyver
+from syringe import Syringe
 
 
 class GameGUI:
@@ -25,57 +25,134 @@ class GameGUI:
     The GameGUI class consists of 2 methods :
         - __init__()
         - run()
-    It needs pygame library and Labyrinth, MacGyver and Syringe imported classes.
 
     """
     def __init__(self):
-        self.labyrinth = Labyrinth("labyrinth_test_GUI.json")
+        """ GameGUI constructor.
+
+        Instantiates objects from Labyrinth, MacGyver and Syringe classes.
+
+        """
+        self.labyrinth = Labyrinth("labyrinth.json")
         self.macgyver = MacGyver(self.labyrinth)
         self.syringe = Syringe(self.labyrinth)
 
     def run(self):
+        """ Manages the game progress with Pygame."""
         pygame.init()
-
         window = pygame.display.set_mode((600, 600))
 
-        background = pygame.image.load("background_450_450.jpg").convert()
+        #Window icon
+        icone = pygame.image.load("images/macgyver_32_40.png")
+        pygame.display.set_icon(icone)
+
+        #Window title
+        pygame.display.set_caption("Help MacGyver to escape from the \
+labyrinth !")
+
+        background = pygame.image.load("images/background_800_545.jpg").convert()
         window.blit(background, (0, 0))
-        window.blit(background, (160, 0))
-        window.blit(background, (0, 160))
-        window.blit(background, (160, 160))
+        window.blit(background, (0, 100))
 
-        macgyver = pygame.image.load("macgyver_32_40.png").convert_alpha()
+        macgyver = pygame.image.load("images/macgyver_32_40.png").convert_alpha()
         macgyver_position = self.labyrinth.get_position("M")
-        # x et y sont inversés dans mes classes ...
-        window.blit(macgyver, (macgyver_position[1] * 40 + 4, macgyver_position[0] * 40))
+        window.blit(macgyver, (macgyver_position[0] * 40 + 4,
+                               macgyver_position[1] * 40))
 
-        guard = pygame.image.load("guard_32_36.png").convert_alpha()
+        guard = pygame.image.load("images/guard_32_36.png").convert_alpha()
         guard_position = self.labyrinth.get_position("G")
-        window.blit(guard, (guard_position[1] * 40 + 4, guard_position[0] * 40 + 2))
+        window.blit(guard, (guard_position[0] * 40 + 4, guard_position[1] *
+                            40 + 2))
 
-        needle = pygame.image.load("needle_20_20.png").convert_alpha()
-        tube = pygame.image.load("tube_20_20.png").convert_alpha()
-        ether = pygame.image.load("ether_20_20.png").convert_alpha()
-        syringe_elements_positions = self.syringe.dispatch()
-        window.blit(needle, (syringe_elements_positions[0][1] * 40 + 10, syringe_elements_positions[0][0] * 40 + 10))
-        window.blit(tube, (syringe_elements_positions[1][1] * 40 + 10, syringe_elements_positions[1][0] * 40 + 10))
-        window.blit(ether, (syringe_elements_positions[2][1] * 40 + 10, syringe_elements_positions[2][0] * 40 + 10))
+        needle = pygame.image.load("images/needle_20_20.png").convert_alpha()
+        tube = pygame.image.load("images/tube_20_20.png").convert_alpha()
+        ether = pygame.image.load("images/ether_20_20.png").convert_alpha()
+        syringe_elements_positions = self.syringe.positions
 
-        wall = pygame.image.load("wall_40_40.png").convert()
+        window.blit(needle, (syringe_elements_positions[0][0] * 40 + 10,
+                             syringe_elements_positions[0][1] * 40 + 10))
+        window.blit(tube, (syringe_elements_positions[1][0] * 40 + 10,
+                           syringe_elements_positions[1][1] * 40 + 10))
+        window.blit(ether, (syringe_elements_positions[2][0] * 40 + 10,
+                            syringe_elements_positions[2][1] * 40 + 10))
+
+        wall = pygame.image.load("images/wall_40_40.png").convert()
 
         walls_positions = self.labyrinth.get_walls_positions()
         for wall_position in walls_positions:
-            window.blit(wall, (wall_position[1] * 40, wall_position[0] * 40))
+            window.blit(wall, (wall_position[0] * 40, wall_position[1] * 40))
 
         pygame.display.flip()
 
+        pygame.key.set_repeat(400, 30)
+
         carry_on = True
+        success = None
 
         while carry_on:
+
+            #Loop speed
+            pygame.time.Clock().tick(30)
+
             for event in pygame.event.get():
                 if event.type == QUIT:
                     carry_on = False
+                elif event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        success = self.macgyver.move("u")
+                    elif event.key == K_DOWN:
+                        success = self.macgyver.move("d")
+                    elif event.key == K_LEFT:
+                        success = self.macgyver.move("l")
+                    elif event.key == K_RIGHT:
+                        success = self.macgyver.move("r")
+
+                window.blit(background, (0, 0))
+                window.blit(background, (0, 100))
+
+                macgyver_position = self.macgyver.position
+                window.blit(macgyver, (macgyver_position.x * 40 + 4,
+                                       macgyver_position.y * 40))
+
+                window.blit(guard, (guard_position[0] * 40 + 4,
+                                    guard_position[1] * 40 + 2))
+
+                if "N" not in self.macgyver.collected_syringe_elements:
+                    window.blit(needle, (syringe_elements_positions[0][0] *
+                                         40 + 10,
+                                         syringe_elements_positions[0][1] *
+                                         40 + 10))
+                if "T" not in self.macgyver.collected_syringe_elements:
+                    window.blit(tube, (syringe_elements_positions[1][0] * 40 +
+                                       10,
+                                       syringe_elements_positions[1][1] * 40 +
+                                       10))
+                if "E" not in self.macgyver.collected_syringe_elements:
+                    window.blit(ether, (syringe_elements_positions[2][0] * 40 +
+                                        10,
+                                        syringe_elements_positions[2][1] * 40 +
+                                        10))
+
+                walls_positions = self.labyrinth.get_walls_positions()
+                for wall_position in walls_positions:
+                    window.blit(wall, (wall_position[0] * 40,
+                                       wall_position[1] * 40))
+
+                pygame.display.flip()
+
+            if success:
+                you_win = pygame.image.load("images/you_win_200_200.png").convert()
+                window.blit(you_win, (200, 200))
+                pygame.display.flip()
+                carry_on = False
+            elif success == False:
+                game_over = pygame.image.load("images/game_over_320_256.jpg").convert()
+                window.blit(game_over, (140, 172))
+                pygame.display.flip()
+                carry_on = False
+
         pygame.quit()
+        pygame.time.wait(4000)
 
 
 def main():
