d57e8924979b9b57ac18d4d3c0a5b112825d2514
Laure <lauremenaut@yahoo.fr>
2018-01-10 16:42:42 -0500

    Added argparse - Added / cleaned comments

--- gametext.py
+++ gametext.py
@@ -5,11 +5,10 @@
 
 This class manages the "textual version" of the game "Help MacGyver to
 escape from the labyrinth !"
-McGyver needs to find and pick up 3 items (a needle, a small plastic
-tube and ether) in the labyrinth to make a syringe. Then, he will be
-able to neutralize the guard and escape.
 
 """
+
+import argparse
 
 from labyrinth import Labyrinth
 from macgyver import MacGyver
@@ -24,13 +23,13 @@ class GameText:
         - run()
 
     """
-    def __init__(self):
+    def __init__(self, json_file="labyrinth.json"):
         """ GameText constructor.
 
         Instantiates objects from Labyrinth, MacGyver and Syringe classes.
 
         """
-        self.labyrinth = Labyrinth("labyrinth_test.json")
+        self.labyrinth = Labyrinth(json_file)
         self.macgyver = MacGyver(self.labyrinth)
         self.syringe = Syringe(self.labyrinth)
 
@@ -46,7 +45,7 @@ class GameText:
         carry_on = True
         print("\nHelp MacGyver to escape from the labyrinth : he needs to pick\
  up a needle, a tube and ether before fighting the guard !")
-        while carry_on is not False:
+        while carry_on:
             self.labyrinth.display()
             user_answer = input("In which direction do you want MacGyver to \
 move ? Please enter 'u' for up, 'd' for down, 'l' for left and 'r' for \
@@ -59,13 +58,24 @@ right, or 'q' to quit : ")
                 carry_on = False
             else:
                 success = self.macgyver.move(user_answer)
-                if success or success == False:
+                # 'success' becomes True or False after fighting the guard
+                if success is not None:
                     carry_on = False
                     self.labyrinth.display()
 
 
+def parse_arguments():
+    """ Returns an arguments parser with a "json_file" argument. """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("json_file", help="Choose a .json file containing a \
+                        labyrinth to load")
+    return parser.parse_args()
+
+
 def main():
-    game = GameText()
+    """ Runs the game """
+    argument = parse_arguments()
+    game = GameText(argument.json_file)
     game.run()
 
 if __name__ == "__main__":
