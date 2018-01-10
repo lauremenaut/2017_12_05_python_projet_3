bd9a37282ee4e7546705803b97c31316b4c8d1cb
Laure <lauremenaut@yahoo.fr>
2018-01-10 16:37:57 -0500

    Added / cleaned comments

--- syringe.py
+++ syringe.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
 """ Sets Syringe class.
 
 Syringe class is imported in gametext.py and gamegui.py files.
@@ -23,27 +26,31 @@ class Syringe:
         Sets a Labyrinth object as an attribute.
         Sets a list of syringe elements.
         Calls :
-            - display()
+            - dispatch()
             - labyrinth.place_syringe_element()
 
         """
         self.labyrinth = labyrinth
         self.syringe_elements = ["N", "T", "E"] # Needle, Tube, Ether
         self.positions = self.dispatch()
-
-        for self.syringe_element, self.position in zip(self.syringe_elements, self.positions):
-            self.labyrinth.place_syringe_element(self.syringe_element, self.position)
+        # Assigns a position to each element
+        for self.syringe_element, self.position in zip(self.syringe_elements,
+                                                       self.positions):
+            self.labyrinth.place_syringe_element(self.syringe_element,
+                                                 self.position)
 
     def dispatch(self):
-        """ Returns a list of tuples of random positions for syringe elements. """
+        """ Returns a list of tuples of random positions for the elements. """
         available_positions = self.labyrinth.get_available_positions()
         return random.sample(available_positions, len(self.syringe_elements))
+
 
 def main():
     labyrinth = Labyrinth("labyrinth_test.json")
     syringe = Syringe(labyrinth)
-    print(syringe.positions) # renvoie la liste des 3 positions choisies
-    labyrinth.display() # affiche le labyrinthe contenant les 3 objets
+    print("Syringe elements positions : ", syringe.positions) # returns a list
+    # of random available positions
+    labyrinth.display() # displays the labyrinth (with the syringe elements)
 
 if __name__ == "__main__":
     main()
